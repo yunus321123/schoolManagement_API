9ec2cf35c628a51d68c7ec58576ce5842980642b
Yunus <118502561+yunus321123@users.noreply.github.com>
2025-08-06 22:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
 });
+
